c693196c2afed12837a3201c67fa5a6d657bed73
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2025-06-23 09:19:54 +0200

    chore: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+</svelte:head>
+
 <div id="svelte" class="flex flex-col min-h-screen">
 	<slot />
 </div>
