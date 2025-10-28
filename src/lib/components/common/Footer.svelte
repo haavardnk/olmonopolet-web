2e0097a6973b4d95dbfcb00dcb0eaf2e789d91e2
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2025-10-28 19:10:52 +0100

    chore: clean up footer

--- src/lib/components/common/Footer.svelte
+++ src/lib/components/common/Footer.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
-	import { PUBLIC_SITE_TITLE } from '$env/static/public';
-	import { Beer } from '@lucide/svelte';
+	import {
+		PUBLIC_SITE_TITLE,
+		PUBLIC_PATREON_URL,
+		PUBLIC_FACEBOOK_URL,
+		PUBLIC_GITHUB_LINK,
+		PUBLIC_GITHUB_LICENSE_LINK
+	} from '$env/static/public';
+	import { Beer, HeartHandshake, Facebook, Github } from '@lucide/svelte';
 </script>
 
-<footer class="footer footer-center p-10 bg-base-200 text-base-content mt-auto">
-	<div>
-		<Beer size={36} class="text-primary" />
-		<p class="font-bold">
-			{PUBLIC_SITE_TITLE} <br />
-		</p>
-		<p>
+<footer class="footer footer-horizontal footer-center bg-base-200 text-base-content p-10 gap-6">
+	<nav>
+		<div class="grid grid-flow-col gap-4">
+			<a href={PUBLIC_PATREON_URL} target="_blank" rel="noopener noreferrer" aria-label="Patreon">
+				<HeartHandshake size={20} strokeWidth={2.5} />
+			</a>
+			<a href={PUBLIC_FACEBOOK_URL} target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+				<Facebook size={20} strokeWidth={2.5} />
+			</a>
+			<a href={PUBLIC_GITHUB_LINK} target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+				<Github size={20} strokeWidth={2.5} />
+			</a>
+		</div>
+	</nav>
+	<aside>
+		<p class="text-sm">
 			© {new Date().getFullYear()}
-			{PUBLIC_SITE_TITLE}. Alle rettigheter reservert.
+			{PUBLIC_SITE_TITLE}
 		</p>
-		<p class="opacity-70">Denne tjenesten er ikke tilknyttet Vinmonopolet.</p>
-	</div>
+	</aside>
 </footer>
